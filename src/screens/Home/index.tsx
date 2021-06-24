5535c6d84030afb226569c860394505b7cee9990
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2021-06-24 15:30:45 -0300

    Criação da página Home

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { View, FlatList, Text } from "react-native";
+
+import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
+import { ListHeader } from "../../components/ListHeader";
+import { Appointment } from "../../components/Appointment";
+import { ListDivider } from "../../components/ListDivider";
+
+import { styles } from "./styles";
+
+export function Home(){
+    const [category, setCategory] = useState('');
+
+    function handleCategorySelect(categoryId: string){
+        categoryId === category ? setCategory('') : setCategory(categoryId)
+    }
+
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'Descrição'
+        },
+        {
+            id: '2',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: false
+            },
+            category: '1',
+            date: '22/06 às 20:40h',
+            description: 'Descrição'
+        }
+    ];
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Profile />
+                <ButtonAdd />
+            </View>
+
+            <View>
+                <CategorySelect 
+                    categorySelected={category}
+                    setCategory={handleCategorySelect}
+                />
+
+                <View style={styles.content}>
+                    <ListHeader 
+                        title="Partidas agendadas"
+                        subtitle="Total: 6"
+                    />
+
+                    <FlatList 
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({item}) => (
+                            <Appointment 
+                                data={item}
+                            />
+                        )}
+                        style={styles.matches}
+                        showsVerticalScrollIndicator={false}
+                        ItemSeparatorComponent={() => <ListDivider />}
+                    />
+                </View>
+            </View>
+        </View>
+    )
+}
